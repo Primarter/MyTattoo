303663d25745344335974744ed472103f976af02
Primarter <alec.ferrari.pro@gmail.com>
2022-05-13 17:15:35 +0200

    Shapes are done, just need to tweak it the right way

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,15 @@
 struct Polygon
 {
     sf::VertexArray va;
+    int size = 100;
+
+    Polygon(void) = default;
 
     Polygon(sf::Vector2f center, sf::Color centerCol, size_t subdivs, float size) {
         va = sf::VertexArray(sf::TriangleFan, subdivs + 2);
         va[0].position = center;
         va[0].color = centerCol;
+        this->size = size;
         for (int i = 1; i <= subdivs + 1; i++) {
             double angle = (2*M_PI)/subdivs * (i - 1);
             va[i].position = center + sf::Vector2f(cos(angle) * fabs(size), sin(angle) * fabs(size));
@@ -27,10 +31,18 @@ struct Polygon
             va[i].color = HSVtoRGB(TO_DEG(angle), .75, .75);
         }
     }
+    void rotate(float angleOffset = 0) {
+        size_t subdivs = va.getVertexCount() - 2;
+        for (int i = 1; i <= subdivs + 1; i++) {
+            double angle = (2*M_PI)/subdivs * (i - 1) + angleOffset;
+            va[i].position = va[0].position + sf::Vector2f(cos(angle) * size, sin(angle) * size);
+            va[i].color = HSVtoRGB(TO_DEG(angle), .75, .75);
+        }
+    }
 };
 
 #define SUBDIVS 50
-#define SIZE 450
+#define SIZE 100.0
 #define VP_WIDTH 1920
 #define VP_HEIGHT 1080
 
@@ -40,8 +52,21 @@ int main(void)
     RenderWindow win(VideoMode(VP_WIDTH, VP_HEIGHT), "Tattoo");
     Vector2f center = Vector2f(VP_WIDTH/2, VP_HEIGHT/2);
     Clock cl;
-    Polygon pol(center, Color(100, 100, 100), SUBDIVS, SIZE);
 
+    Polygon polArr[5];
+
+    CircleShape circle;
+
+    float inradius = SIZE;
+    for (int i = 0; i < 5; i++) {
+        inradius = inradius/cos(M_PI/(i+3));
+        polArr[i] = Polygon(center, Color(125/(i+1), 125/(i+1), 125/(i+1)), i + 3, inradius);
+    }
+
+    // circle.setFillColor(Color::Black);
+    circle.setFillColor(Color(0x00000088));
+    // circle.setOutlineColor(Color::Black);
+    // circle.setOutlineThickness(1);
     cl.restart();
     while (win.isOpen()) {
         Event e;
@@ -49,9 +74,18 @@ int main(void)
             if (e.type == Event::Closed || (e.type == Event::KeyReleased && e.key.code == Keyboard::Escape))
                 win.close();
         }
-        pol.update(center, Color(100, 100, 100), SIZE, cl.getElapsedTime().asSeconds()/4);
         win.clear();
-        win.draw(pol.va);
+        float radius = inradius;
+        for (int i = 4; i >= 0; i--) {
+            radius = radius*cos(M_PI/(i+3));
+            polArr[i].rotate(cl.getElapsedTime().asSeconds()/4);
+            circle.setPosition(center - Vector2f(radius, radius));
+            circle.setRadius(radius);
+            win.draw(polArr[i].va);
+            win.draw(circle);
+        }
+        // pol.update(center, Color(100, 100, 100), SIZE, cl.getElapsedTime().asSeconds()/4);
+        // win.draw(pol.va);
         win.display();
     }
 }
